12099f9779d309558b52c35c4d1b3aa2855fa00c
Administrator <Administrator@172.20.0.54>
2024-04-12 11:43:18 +0530

    this is addition program

---
+++ src/com/Package/addition.java
@@ -0,0 +1,11 @@
+package com.Package;
+
+public class addition {
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		int c=a+b;
+		System.out.println("Addition is "+c);
+				
+	}
+}
